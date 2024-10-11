d0779b14bf02439a10b763d7f3fb310a2b639d76
YinanDanielZhou <zhouyinan49@gmail.com>
2024-10-11 11:39:05 -0700

    fix

--- src/SDB/blockchainSynchronizer/localConsistencyCheck.ts
+++ src/SDB/blockchainSynchronizer/localConsistencyCheck.ts
@@ -24,11 +24,11 @@ let known_block_height: number;
 
     if (needToMakeUp) {
         const missedTxnList = [
-           //  "3c2048fd6df593a1d60f066f456ddd03234d1fb39fbba4b822752892f160dd54",
-           //  "da82527c5d6e463e6d9b458e54e5a13cb3c4757db6b7b036de73100b3e599ffa",
-           //  "58502d8372546dd3a87eca582ee268a84d0a6065136cd2d93dd360b314ebdf1f",
-           //  "34be19216cca8473ace002b27efede783c83660a2bbef893f6e18b5a916fdbdb",
-           //  "7e900211abf97e1ce134c1b3576263179018794c864aa403f66524d3328c5fd2"
+            "6c1f0b452ae6a5e10d78b85599ef07560f8a3255e33c083a0ace932f8304d86c",
+            "ea5ffa720b85ae9b6ea0764b110583de16a0e2612e835c2bad8fad301b59ca1a",
+           //  "1b9f2a32fe34c30ce993ef3ebbc9be2e10e7b83f22d08585c787c09722bf0b0d",
+           //  "ea31378cf9ae29d97132495b5977b6003357515756e95219d0d350d863ab6b73",
+           //  "0d8e6a8e7b527bdb208c1c51ca226d7ae1081a83219fe22edc063655ad24b3d4"
         ]
         for (let txid of missedTxnList) {
             await makeupMissingTxn(txid, SDO_curr_state)
