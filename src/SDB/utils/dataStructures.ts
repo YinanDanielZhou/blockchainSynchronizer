640ed0939cc684ab06de20054076337cac0aa499
YinanDanielZhou <zhouyinan49@gmail.com>
2026-01-22 09:57:38 -0800

    folder rename

--- src/SDB/utils/dataStructures.ts
+++ src/SDB/utils/dataStructures.ts
@@ -1,7 +1,8 @@
-import { SpendableDO } from "../../contracts/SpendableDO";
+// import { SpendableDO } from "../../contracts/SpendableDO";
+import { SDOOwnerWriter } from "../../contracts/SDO_v7";
 
 export interface LLNodeSDO {
-    this: SpendableDO,
+    this: SDOOwnerWriter,
     prev?: LLNodeSDO,
     next?: LLNodeSDO,
     block_time: number  // a timestamp given to a txn when it is first heard by a peer node
